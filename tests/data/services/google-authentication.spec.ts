c283f5bd901657b6d9225b7cd2229f939e555a02
William <william_ewerton@outlook.com>
2022-05-20 13:18:42 -0300

    change test strategy

--- tests/data/services/google-authentication.spec.ts
+++ tests/data/services/google-authentication.spec.ts
@@ -1,32 +1,26 @@
 import { AuthenticationError } from '@/domain/errors'
-import { LoadGoogleUserApi } from '@/data/contracts/apis'
 import { GoogleAuthenticationService } from '@/data/contracts/apis/services'
-
-class LoadGoogleUserApiSpy implements LoadGoogleUserApi {
-  token?: string
-  callsCount = 0
-  result = undefined
-  async loadUser (params: LoadGoogleUserApi.Params): Promise<LoadGoogleUserApi.Result> {
-    this.token = params.token
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('GoogleAuthenticationService', () => {
   it('Should call LoadGoogleUserApi with correct params', async () => {
-    const loadGoogleUserApi = new LoadGoogleUserApiSpy()
+    const loadGoogleUserApi = {
+      loadUser: jest.fn()
+    }
+
     const sut = new GoogleAuthenticationService(loadGoogleUserApi)
 
     await sut.perform({ token: 'any_token' })
 
-    expect(loadGoogleUserApi.token).toBe('any_token')
-    expect(loadGoogleUserApi.callsCount).toBe(1)
+    expect(loadGoogleUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadGoogleUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadGoogleUserApi returns undefined', async () => {
-    const loadGoogleUserApi = new LoadGoogleUserApiSpy()
-    loadGoogleUserApi.result = undefined
+    const loadGoogleUserApi = {
+      loadUser: jest.fn()
+    }
+
+    loadGoogleUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new GoogleAuthenticationService(loadGoogleUserApi)
 
     const authResult = await sut.perform({ token: 'any_token' })
