64b695540bb42438b0b16f7761ab356928379982
William <william_ewerton@outlook.com>
2022-05-20 13:41:55 -0300

    simplify code

--- tests/data/services/google-authentication.spec.ts
+++ tests/data/services/google-authentication.spec.ts
@@ -3,36 +3,27 @@ import { LoadGoogleUserApi } from '@/data/contracts/apis'
 import { GoogleAuthenticationService } from '@/data/contracts/apis/services'
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type SutTypes = {
-  sut: GoogleAuthenticationService
-  loadGoogleUserApi: MockProxy<LoadGoogleUserApi>
-}
-
-const makeSut = (): SutTypes => {
-  const loadGoogleUserApi = mock<LoadGoogleUserApi>()
-  const sut = new GoogleAuthenticationService(loadGoogleUserApi)
-
-  return {
-    sut,
-    loadGoogleUserApi
-  }
-}
-
 describe('GoogleAuthenticationService', () => {
-  it('Should call LoadGoogleUserApi with correct params', async () => {
-    const { sut, loadGoogleUserApi } = makeSut()
-    await sut.perform({ token: 'any_token' })
+  let loadGoogleUserApi: MockProxy<LoadGoogleUserApi>
+  let sut: GoogleAuthenticationService
+  const token = 'any_token'
 
-    expect(loadGoogleUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+  beforeEach(() => {
+    loadGoogleUserApi = mock()
+    sut = new GoogleAuthenticationService(loadGoogleUserApi)
+  })
+
+  it('Should call LoadGoogleUserApi with correct params', async () => {
+    await sut.perform({ token })
+
+    expect(loadGoogleUserApi.loadUser).toHaveBeenCalledWith({ token })
     expect(loadGoogleUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadGoogleUserApi returns undefined', async () => {
-    const { sut, loadGoogleUserApi } = makeSut()
-
     loadGoogleUserApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
