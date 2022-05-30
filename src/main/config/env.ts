7c3083cb9c9c4c8a7e40d228f5e1ebd5688cceae
William <william_ewerton@outlook.com>
2022-05-30 10:23:16 -0300

    config express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '962950074392514',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '486a8df296d74bdf7d07adfa53b852ac'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
