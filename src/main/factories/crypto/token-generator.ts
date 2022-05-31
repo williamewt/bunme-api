dda32af998b1cba7f26bf540f4309dedde51f3ef
William <william_ewerton@outlook.com>
2022-05-31 11:42:03 -0300

    JwtTokenGenerator to JwtTokenHandler

--- src/main/factories/crypto/token-generator.ts
+++ src/main/factories/crypto/token-generator.ts
@@ -1,7 +1,7 @@
 
-import { JwtTokenGenerator } from '@/infra/crypto'
+import { JwtTokenHandler } from '@/infra/crypto'
 import { env } from '@/main/config/env'
 
-export const makeJwtTokenGenerator = (): JwtTokenGenerator => {
-  return new JwtTokenGenerator(env.jwtSecret)
+export const makeJwtTokenHandler = (): JwtTokenHandler => {
+  return new JwtTokenHandler(env.jwtSecret)
 }
--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -2,12 +2,12 @@
 import { FacebookAuthentication, setupFacebookAuthentication } from '@/domain/use-cases'
 import { makeFacebookApi } from '@/main/factories/apis'
 import { makePgUserAccountRepository } from '@/main/factories/repos/postgres'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepository(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
